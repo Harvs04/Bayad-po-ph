471d8779704e51c6bbe9e4a5e8348aca69e3f0b7
Harvs04 <hamartinez@up.edu.ph>
2025-08-12 19:52:07 +0800

    feat: added check fare route

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LocationSearch from './pages/LocationSearch.jsx'
 
 const router = createBrowserRouter([
-  {path: '/', element: <App />}
+  {path: '/', element: <App />},
+  {path: '/check-fare', element: <LocationSearch />} 
 ]);
 
 createRoot(document.getElementById('root')).render(
