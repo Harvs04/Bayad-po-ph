ec3cbe341fc64aa6fe5156d168ebfc9f4370c762
Harvs04 <hamartinez@up.edu.ph>
2025-08-13 20:49:34 +0800

    feat: added use current location feature

--- src/pages/LocationSearch.jsx
+++ src/pages/LocationSearch.jsx
@@ -322,13 +322,30 @@ const LocationSearch = () => {
                 type="text"
                 value={startLocation}
                 onChange={(e) => handleChangeInput(e, 0)}
-                className="p-2 pr-8 text-[15px] text-[#4d4d4d] border border-[#4d4d4d] rounded-md w-full"
+                className="p-2 pr-16 text-[15px] text-[#4d4d4d] border border-[#4d4d4d] rounded-md w-full"
               />
 
               <button
                 type="button"
+                onClick={() => reverseGeocoding(0, currentLoc.latitude, currentLoc.longitude)}
+                className="absolute right-8 top-1/2 -translate-y-1/2 cursor-pointer hover:bg-blue-50 rounded p-1 transition-colors"
+                title="Use current location"
+              >
+                <svg
+                  fill="#2196F3"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="w-5 h-5"
+                >
+                  <path d="M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8M3.05,13H1V11H3.05C3.5,6.83 6.83,3.5 11,3.05V1H13V3.05C17.17,3.5 20.5,6.83 20.95,11H23V13H20.95C20.5,17.17 17.17,20.5 13,20.95V23H11V20.95C6.83,20.5 3.5,17.17 3.05,13M12,5A7,7 0 0,0 5,12A7,7 0 0,0 12,19A7,7 0 0,0 19,12A7,7 0 0,0 12,5Z" />
+                </svg>
+              </button>
+
+              <button
+                type="button"
                 onClick={handleDeleteOrigin}
-                className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer"
+                title="Remove origin"
+                className="absolute right-1 top-1/2 -translate-y-1/2 cursor-pointer hover:bg-red-50 rounded p-1 transition-colors"
               >
                 <svg
                   fill="#EB2020"
@@ -388,13 +405,30 @@ const LocationSearch = () => {
                 type="text"
                 value={endLocation}
                 onChange={(e) => handleChangeInput(e, 1)}
-                className="p-2 pr-8 text-[15px] text-[#4d4d4d] border border-[#4d4d4d] rounded-md w-full"
+                className="p-2 pr-16 text-[15px] text-[#4d4d4d] border border-[#4d4d4d] rounded-md w-full"
               />
 
               <button
                 type="button"
+                onClick={() => reverseGeocoding(1, currentLoc.latitude, currentLoc.longitude)}
+                className="absolute right-8 top-1/2 -translate-y-1/2 cursor-pointer hover:bg-blue-50 rounded p-1 transition-colors"
+                title="Use current location"
+              >
+                <svg
+                  fill="#2196F3"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="w-5 h-5"
+                >
+                  <path d="M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8M3.05,13H1V11H3.05C3.5,6.83 6.83,3.5 11,3.05V1H13V3.05C17.17,3.5 20.5,6.83 20.95,11H23V13H20.95C20.5,17.17 17.17,20.5 13,20.95V23H11V20.95C6.83,20.5 3.5,17.17 3.05,13M12,5A7,7 0 0,0 5,12A7,7 0 0,0 12,19A7,7 0 0,0 19,12A7,7 0 0,0 12,5Z" />
+                </svg>
+              </button>
+
+              <button
+                type="button"
                 onClick={handleDeleteDestination}
-                className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer"
+                title="Remove destination"
+                className="absolute right-1 top-1/2 -translate-y-1/2 cursor-pointer hover:bg-red-50 rounded p-1 transition-colors"
               >
                 <svg
                   fill="#EB2020"
