40852b6a2c094be1fc657c7f8c2a10d2c9b71132
Harvs04 <hamartinez@up.edu.ph>
2025-08-13 22:25:05 +0800

    feat: recenter to selected origin location

--- src/components/MapArea.jsx
+++ src/components/MapArea.jsx
@@ -5,66 +5,97 @@ import {
   Popup,
   Polyline,
   useMapEvents,
+  useMap,
 } from "react-leaflet";
 import L from "leaflet";
+import { useEffect } from "react";
 import markerIconPng from "leaflet/dist/images/marker-icon.png";
 
-const MapArea = ({ markers, setMarkers, reverseGeocoding, setOriginSuggestions, setIsOriginPinned, setDestinationSuggestions, setIsDestinationPinned, route }) => {
+const RecenterToOrigin = ({ markers }) => {
+  const map = useMap();
 
-    const MapClickHandler = () => {
-        useMapEvents({
-          click(e) {
-            if (markers.length < 2) {
-              setMarkers([...markers, [e.latlng.lat, e.latlng.lng]]);
-              if (markers.length === 0) {
-                reverseGeocoding(0, e.latlng.lat, e.latlng.lng);
-                setOriginSuggestions([]);
-                setIsOriginPinned(true);
-              } else if (markers.length === 1) {
-                reverseGeocoding(1, e.latlng.lat, e.latlng.lng);
-                setDestinationSuggestions([]);
-                setIsDestinationPinned(true);
-              }
-            }
-          },
-        });
-        return null;
-    };
+  useEffect(() => {
+    if (Array.isArray(markers[0]) && markers[0].length === 2) {
+      map.setView(markers[0], map.getZoom());
+    }
+  }, [markers[0], map]);
 
-    return (
-        <>
-        <MapContainer
-            center={[14.693099, 121.058873]}
-            zoom={14}
-            scrollWheelZoom={true}
-            style={{ height: "100%", width: "100%" }}
-        >
-            <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+  return null;
+};
 
-            <MapClickHandler />
+const MapArea = ({
+  markers,
+  setMarkers,
+  reverseGeocoding,
+  setOriginSuggestions,
+  setIsOriginPinned,
+  setDestinationSuggestions,
+  setIsDestinationPinned,
+  route,
+}) => {
+  const MapClickHandler = () => {
+    useMapEvents({
+      click(e) {
+        if (markers[0] === null || markers[1] === null) {
+          if (markers[0] === null) {
+            setMarkers((prev) => {
+              prev[0] = [e.latlng.lat, e.latlng.lng];
+              return [...prev];
+            });
+            reverseGeocoding(0, e.latlng.lat, e.latlng.lng);
+            setOriginSuggestions([]);
+            setIsOriginPinned(true);
+          } else if (markers[1] === null) {
+            setMarkers((prev) => {
+              prev[1] = [e.latlng.lat, e.latlng.lng];
+              return [...prev];
+            });
+            reverseGeocoding(1, e.latlng.lat, e.latlng.lng);
+            setDestinationSuggestions([]);
+            setIsDestinationPinned(true);
+          }
+        }
+      },
+    });
+    return null;
+  };
 
-            {markers.map((pos, idx) => (
+  return (
+    <>
+      <MapContainer
+        center={[14.693099, 121.058873]}
+        zoom={15}
+        scrollWheelZoom={true}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <RecenterToOrigin markers={markers} />
+        <MapClickHandler />
+
+        {markers
+          .filter((pos) => Array.isArray(pos) && pos.length === 2)
+          .map((pos, idx) => (
             <Marker
-                key={idx}
-                position={pos}
-                icon={L.icon({
+              key={idx}
+              position={pos}
+              icon={L.icon({
                 iconUrl: markerIconPng,
                 iconSize: [25, 41],
                 iconAnchor: [12, 41],
-                })}
+              })}
             >
-                <Popup>Point {idx + 1}</Popup>
+              <Popup>Point {idx + 1}</Popup>
             </Marker>
-            ))}
+          ))}
 
-            {route.length > 0 && <Polyline positions={route} color="blue" />}
-        </MapContainer>
-        </>
-    );
+        {route.length > 0 && <Polyline positions={route} color="blue" />}
+      </MapContainer>
+    </>
+  );
 };
-
 
 export default MapArea;
